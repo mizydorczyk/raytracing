72de27b063eaa162862bfb781c8097b9823cada0
mizydorczyk <matizydorczyk@outlook.com>
2026-04-06 21:44:01 +0200

    Add color writing utility

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::io::Write;
 
+use raytracing::vec3::Color3;
+
+fn write_color(color: &Color3) {
+    let r = (color[0].clamp(0.0, 1.0) * 255.0).round() as u8;
+    let g = (color[1].clamp(0.0, 1.0) * 255.0).round() as u8;
+    let b = (color[2].clamp(0.0, 1.0) * 255.0).round() as u8;
+
+    println!("{r} {g} {b}");
+}
+
 fn main() {
     let width = 256;
     let height = 256;
@@ -17,11 +27,8 @@ fn main() {
             let g = j as f64 / (height - 1) as f64;
             let b = 0.0f64;
 
-            let r = (r.clamp(0.0, 1.0) * 255.0).round() as u8;
-            let g = (g.clamp(0.0, 1.0) * 255.0).round() as u8;
-            let b = (b.clamp(0.0, 1.0) * 255.0).round() as u8;
-
-            println!("{r} {g} {b}");
+            let color = Color3::new(r, g, b);
+            write_color(&color);
         }
     }
 
